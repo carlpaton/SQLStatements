2af9374424eca5fb17a30bff3d1ba99595c2785a
Carl Paton <cpaton@trademe.local>
2018-12-18 13:08:14 +1300

    Cleanup mysql flyway scripts

--- mssql/test/table/V1.0.0__staff_master.sql
+++ mssql/test/table/V1.0.0__staff_master.sql
@@ -1,7 +1,8 @@
-CREATE TABLE staff_master (
-  id int IDENTITY(1,1) PRIMARY KEY,
-  first_name VARCHAR(45) NULL,
-  surname VARCHAR(45) NULL,
-  email VARCHAR(45) NULL,
-  insert_date DATETIME NULL,
-  salary FLOAT);
+  CREATE TABLE `test`.`staff_master` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `surname` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `insert_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `salary` FLOAT NULL,
+  PRIMARY KEY (`id`));
