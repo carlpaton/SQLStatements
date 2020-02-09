a4b54bbcb84935070952ca016da0e67961f89f4c
Carl <carl.paton@gmail.com>
2020-02-10 11:37:07 +1300

    oss_index reference increase column size

--- mssql/VulnusCloud/V1.0.3__oss_index.sql
+++ mssql/VulnusCloud/V1.0.3__oss_index.sql
@@ -8,7 +8,7 @@ CREATE TABLE [dbo].[oss_index](
 	[type_format] [varchar](50) NULL,
 	[coordinates] [varchar](50) NULL,	
 	[description] [varchar](500) NULL,
-	[reference] [varchar](100) NULL,
+	[reference] [varchar](200) NULL,
 	[expire_date] [DATETIME],
 	[http_status] [int] NOT NULL
 ) ON [PRIMARY]
